b5b33478fc7d530cd8d01f7a3b786139e9f19970
chauhan sagar <chauhansagargk@gmail.com>
2024-06-22 15:17:11 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom';
 import ProductData from './ProductData';
+import HomePage from './HomePage';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/product/:userId" element={<ProductData />} />
+      <Route path="/" element={<HomePage />} />
     </Routes>
+
   )
 }
 
